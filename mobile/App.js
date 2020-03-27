2be684c228bc5c84e86f31d1370e24fffe942841
Rafael Costa Palma <rafaelcostapalma@gmail.com>
2020-03-26 22:11:53 -0300

    finishing mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,3 +1,6 @@
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -8,12 +11,3 @@ export default function App() {
     <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
